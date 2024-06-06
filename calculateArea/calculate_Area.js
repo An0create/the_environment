e07cb130243bc5883cd638dba84009e7d82df8da
An0create <an0creates@gmail.com>
2024-06-05 21:42:01 -0400

    Updated JS code to display the output

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,13 +1,10 @@
 let length;
 let width;
-function calculateArea() {
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
-}
-function calculateArea() {
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
 
-let area = length * width;
-}
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+   
+   let area = length * width;
+   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+   }
