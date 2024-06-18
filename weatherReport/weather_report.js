8ee9f00d2e84fc0812c2261110282defcfd7ca03
an0create <an0creates@gmail.com>
2024-06-17 23:41:51 -0400

    Created weatherReport.HTML and JS

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,24 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = 'c4f86ece00bc8aa272652ac9065af12d'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;           
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+  const weatherInfo = document.getElementById('weatherInfo');
+  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                          <p>Temperature: ${data.main.temp} &#8451;</p>
+                          <p>Weather: ${data.weather[0].description}</p>`;
+})
+
+.catch(error => {
+    console.error('Error fetching weather:', error);
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+  });
+  
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
